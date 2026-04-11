e6d64f9c610b7b98d4446eaec453ae7bcd1a32c2
André Amorim <contact@andreamorim.site>
2026-04-12 00:54:35 +0100

    Update plugin metadata

--- afca-blocks-library.php
+++ afca-blocks-library.php
@@ -1,10 +1,12 @@
 <?php
 /**
  * Plugin Name: AFCA Blocks Library
+ * Plugin URI: https://andreamorim.site/plugin-documentation/afca-blocks-library/
  * Description: A collection of custom Gutenberg blocks for WordPress: Rank Math Breadcrumbs, Shortcode for Template Parts, Link Group, Meta Field, Share Post Socials and Swiper Slider.
  * Requires at least: 6.8
  * Requires PHP:      8.0
  * Author: André Amorim
+ * Author URI: https://andreamorim.site
  * Version: 2.0
  * Text Domain: afca-blocks-library
  * Domain Path: /languages
